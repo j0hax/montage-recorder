f43cc17fc91f20b3d6a29b0ceefda201e6671853
Johannes Arnold <johannesarnold@protonmail.ch>
2018-11-12 23:42:30 +0100

    Encapsulated main functions

--- record.sh
+++ record.sh
@@ -9,7 +9,7 @@ start_index=0
 time_interval=5
 delay=0
 
-while getopts "o:i:t:yd:" opt; do
+while getopts "o:i:t:yd:h" opt; do
   case "$opt" in
     o)  output_folder=$OPTARG
       ;;
@@ -21,39 +21,60 @@ while getopts "o:i:t:yd:" opt; do
       ;;
     d)  delay=$OPTARG
       ;;
+    h)  print_help
+      ;;
+    s)  stitch_video
+      ;;
     \?) echo "Invalid option: -$OPTARG" >&2
         exit 1
       ;;
   esac
 done
 
-echo "This script records screenshots at regular intervals to a folder"
-echo "Montage recorder configuration:"
-echo " -o Output folder: $output_folder"
-echo " -i Start index:   $start_index"
-echo " -t Time interval: $time_interval"
-echo " -d Delay:         $delay"
+function print_help {
+  echo "This script records screenshots at regular intervals to a folder"
+  echo "Montage recorder configuration:"
+  echo " -o Output folder: $output_folder"
+  echo " -i Start index:   $start_index"
+  echo " -t Time interval: $time_interval"
+  echo " -y Force start (non-interactive)"
+  echo " -d Delay:         $delay"
+}
 
-shift $((OPTIND-1))
+#shift $((OPTIND-1))
 
 if [ -z ${force_start+x} ]; then
-  read -r -p "Looks good? [y/N] " response
+  read -r -p "Ready? [y/N] " response
 
   if ! [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]; then
     exit 1
   fi
 fi
 
-sleep $delay
+function countdown {
+  secs=5
+  while [ $secs -gt 0 ]; do
+    echo -n "$secs... "
+    : $((secs--))
+    sleep 1
+  done
+  echo "Starting recording!"
+}
 
-recorded=$start_index
+countdown
 
-mkdir -p $output_folder
+function begin_recording {
+  recorded=$start_index
 
-while true
-do
-  recorded=$(($recorded+1))
-  screencapture -x "$output_folder/$recorded.png"
-  echo "Recorded screenshot $recorded to $output_folder"
-  sleep $time_interval
-done
+  mkdir -p $output_folder
+
+  while true
+  do
+    recorded=$(($recorded+1))
+    screencapture -x "$output_folder/$recorded.png"
+    echo "Recorded screenshot $recorded to $output_folder"
+    sleep $time_interval
+  done  
+}
+
+begin_recording
